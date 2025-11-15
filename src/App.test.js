960cb69831a789ccc8fdce5b1f5a9274b1daed0a
Haticedv <devecihatice32@gmail.com>
2025-11-15 13:15:46 +0300

    App.js için test oluşturuldu

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Componenti (Ana Uygulama)', () => {
+
+  test('varsayılan olarak Başlangıç Ekranını render etmeli', () => {
+    render(<App />);
+    const baslikElementi = screen.getByText('Oyun Modu Seçin:');
+    expect(baslikElementi).toBeInTheDocument();
+    const kategoriMetni = screen.queryByText(/Kategori:/i);
+    expect(kategoriMetni).not.toBeInTheDocument();
+  });
+
+  test('başlat butonuna tıklandığında Oyun Ekranına geçiş yapmalı', () => {
+    render(<App />);
+
+   ByText(/Oyunu Başlat/i);
+    
+    fireEvent.click(baslatButonu);
+    const baslikElementi = screen.queryByText('Oyun Modu Seçin:');
+    expect(baslikElementi).not.toBeInTheDocument();
+    const kategoriMetni = screen.getByText(/Kategori:/i);
+    expect(kategoriMetni).toBeInTheDocument();
+  });
+  
 });
